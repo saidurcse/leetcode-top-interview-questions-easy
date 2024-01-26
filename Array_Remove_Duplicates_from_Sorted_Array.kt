07e5f5a4f1f156e6f34ee24ab338d98f10373ae1
Muhammad Saidur Rahman <saidurdev16@gmail.com>
2024-01-25 22:39:36 -0600

    New Solution Added

---
+++ Array_Remove_Duplicates_from_Sorted_Array.kt
@@ -0,0 +1,39 @@
+https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+
+fun removeDuplicates(nums: IntArray): Int {
+
+   var count = 1
+
+   for(i in 1 until nums.size) {
+
+       if(nums[i] != nums[i-1]) {
+           nums[count] = nums[i]
+           count++
+       }
+   }
+
+   return count
+}
+
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+fun removeDuplicates(nums: IntArray): Int {
+
+   val set = HashSet<Int>()  // Use 'val' for a non-mutable set
+   var writeIndex = 0       // Track the index for writing unique elements
+
+   for (num in nums) {
+	 // Add returns true if the element was added successfully
+       if (set.add(num)) {
+	     // Write unique elements to the beginning of the array  
+           nums[writeIndex++] = num  
+       }
+   }
+
+   return set.size
+}
+
+// Time complexity: O(n)
+// Space complexity: O(n)
